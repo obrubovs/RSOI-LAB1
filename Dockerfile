d8d1ebb53f9e5de3a57214be480c94e6e6f53cfb
obrubovs <siobrubov@yandex.ru>
2023-10-17 14:56:36 +0300

    docker seted

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM python:3
+
+WORKDIR /app
+COPY . /app
+
+ENV PYTHONUNBUFFERED=1
+
+RUN pip install -r requirements.txt
+
+EXPOSE 8000
+
+CMD ["python", "LAB1/manage.py", "runserver"]
--- LAB1/LAB1/settings.py
+++ LAB1/LAB1/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -83,6 +84,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+DATABASES['default'] = dj_database_url.parse('postgres://rsoi_lab1_db_user:yLUGjzndggnA1Xfs67rqV38TTCg6hC1v@dpg-ckmocnbj89us739r26ag-a.frankfurt-postgres.render.com/rsoi_lab1_db')
+
 
 
 # Password validation

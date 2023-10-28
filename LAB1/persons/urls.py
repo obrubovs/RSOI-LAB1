4379a95b1b428600ddbd6467e9c2e58b60911cb0
obrubovs <siobrubov@yandex.ru>
2023-10-28 21:06:42 +0300

    record id url changed

--- LAB1/persons/urls.py
+++ LAB1/persons/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.records, name='persons'),
     # path('create', views.create, name='create'),
-    path('<int:pk>', views.record_by_id, name='record'),
+    re_path(r'([0-9]+)$', views.record_by_id, name='record'),
 ]

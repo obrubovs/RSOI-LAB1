7f805217bae97942e7ec01f2ec8e18fc88c42396
obrubovs <siobrubov@yandex.ru>
2023-10-29 02:12:10 +0300

    record id url rolled back

--- LAB1/persons/urls.py
+++ LAB1/persons/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.records, name='persons'),
     # path('create', views.create, name='create'),
-    re_path(r'([0-9]+)$', views.record_by_id, name='record'),
+    path('<int:pk>', views.record_by_id, name='record'),
 ]
